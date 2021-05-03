4e04a81508cf6be1a5661d0b30a3569fa3b25574
woori3104 <sizuku11261@gmail.com>
2021-05-03 15:49:26 +0900

    #3.3 POC API

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+    type Movie {
+        title: String
+        year: Int
+    }
     type Query {
-        hello: String
+        movies : [Movie]
+        movie : Movie
+
+    }
+    type Mutation {
+        createMovie(title:String!): Boolean
+        deleteMovie(title:String!): Boolean
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "world",
+        movies: () => [],
+        movie: () => ({title : "Hello", year: 2021 }),
     },
+    Mutation: {
+        createMovie: (_, {title}) => {
+            console.log(title);
+            return true
+        },
+        deleteMovie:  (_, {title}) => {
+            console.log(title);
+            return true
+        },
+    }
 };
 
 const server = new ApolloServer({
