6c638e07ed96f29b1f27ce2abf596dd3d51ffb96
woori3104 <sizuku11261@gmail.com>
2021-05-05 18:13:24 +0900

    #4.3 seeProfile

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -11,32 +11,40 @@ export default {
         password
     }) => 
         {
-            //check if username or email are already on DB. 
-            const existingUser = await client.user.findFirst({
-                where:{ 
-                    OR: [
-                        {
-                            userName, 
-                        }, 
-                        {
-                            email,
-                        }
-                    ],
-                },
-            });
-             // hash password
-            const uglyPassword = await bcrypt.hash(password, 10);
-            return client.user.create({
-                data: {
-                    userName, 
-                    email, 
-                    firstName, 
-                    lastName, 
-                    password: uglyPassword,
-                },
-            });
+            try {
+
+                //check if username or email are already on DB. 
+                const existingUser = await client.user.findFirst({
+                    where:{ 
+                        OR: [
+                            {
+                                userName, 
+                            }, 
+                            {
+                                email,
+                            }
+                        ],
+                    },
+                });
+                if (existingUser) {
+                    throw new Error("This username/password is already taken.");
+                }
+                // hash password
+                const uglyPassword = await bcrypt.hash(password, 10);
+                return client.user.create({
+                    data: {
+                        userName, 
+                        email, 
+                        firstName, 
+                        lastName, 
+                        password: uglyPassword,
+                    },
+                });
+            } catch(e) {
+                return e;
+            }
+            
         },
-   
     // 
     },
 };
