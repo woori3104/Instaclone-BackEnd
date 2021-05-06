b33fbb6f1696377da89fec73c436276860f84547
woori3104 <sizuku11261@gmail.com>
2021-05-06 19:47:49 +0900

    #4.6 Divide and Conquer

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import client from "../client";
+import client from "../../client";
 import jwt from "jsonwebtoken";
 export default {
   Mutation: {
@@ -43,33 +43,6 @@ export default {
             } catch(e) {
                 return e;
             }
-            
-        },
-        // login
-        login : async (_ ,{userName, password}) => 
-        {
-            // find user with args.username
-            const user = await client.user.findFirst({where: {userName}});
-            if (!user) {
-                return {
-                    ok: false,
-                    error: "user not found"
-                };
-            }
-            // check password with args.password
-            const passwordOk = await bcrypt.compare(password, user.password);
-            if (!passwordOk) {
-                return {
-                    ok:false,
-                    error:"Incorrect password"
-                };
-            }
-            // issue a token and send it to the user
-            const token = await jwt.sign({id:user.id}, process.env.SECRET_KEY);
-            return {
-                ok: true,
-                token,
-            };
         },
     },
-};            
+}
