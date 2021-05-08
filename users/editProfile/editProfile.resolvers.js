38f69cd8ef41639b41b03269351a7ac6730887bb
woori3104 <sizuku11261@gmail.com>
2021-05-08 23:07:49 +0900

    #4.13 Recap

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -3,45 +3,39 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
 
-export default 
-{
-    Mutation:
-    {
-        editProfile: protectedResolver
-        (
-            async (
-                _,
-                { firstName, lastName, username, email, password: newPassword },
-                { loggedInUser, protectResolver }
-            ) => 
-                {
-                    let uglyPassword = null;
-                    if (newPassword) {
-                    uglyPassword = await bcrypt.hash(newPassword, 10);
-                    }
-                    const updatedUser = await client.user.update({
-                    where: {
-                        id: loggedInUser.id,
-                    },
-                    data: {
-                        firstName,
-                        lastName,
-                        username,
-                        email,
-                        ...(uglyPassword && { password: uglyPassword }),
-                    },
-                    });
-                    if (updatedUser.id) {
-                    return {
-                        ok: true,
-                    };
-                    } else {
-                    return {
-                        ok: false,
-                        error: "Could not update profile.",
-                    };
-                }
-            }
-        ),
+const resolverFn = async (
+    _,
+    { firstName, lastName, username, email, password: newPassword },
+    { loggedInUser }) => {
+    let uglyPassword = null;
+    if (newPassword) {
+        uglyPassword = await bcrypt.hash(newPassword, 10);
+    }
+    const updatedUser = await client.user.update({
+        where: {
+            id: loggedInUser.id,
+        },
+        data: {
+            firstName,
+            lastName,
+            username,
+            email,
+            ...(uglyPassword && { password: uglyPassword }),
+        },
+    });
+    if (updatedUser.id) {
+            return {
+            ok: true,
+        };
+    } else {
+            return {
+            ok: false,
+            error: "Could not update profile.",
+        };
+    }
+};
+export default {
+    Mutation: {
+        editProfile: protectedResolver(resolverFn),
     },
 };
