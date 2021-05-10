681412eeb562264cfbeff13d02887079ef03a29a
woori3104 <sizuku11261@gmail.com>
2021-05-10 23:49:21 +0900

    #4.15 File Upload part Two

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -5,8 +5,9 @@ import { protectedResolver } from "../users.utils";
 
 const resolverFn = async (
     _,
-    { firstName, lastName, username, email, password: newPassword, bio },
+    { firstName, lastName, username, email, password: newPassword, bio, avatar },
     { loggedInUser }) => {
+    console.log(avatar);
     let uglyPassword = null;
     if (newPassword) {
         uglyPassword = await bcrypt.hash(newPassword, 10);
