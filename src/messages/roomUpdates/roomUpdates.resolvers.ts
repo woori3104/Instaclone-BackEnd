076e78d43bb2bbc0611483357b4a7d4be9af0edb
woori3104 <sizuku11261@gmail.com>
2021-05-30 12:20:39 +0900

    #7.10 Filtering Subscriptions

--- src/messages/roomUpdates/roomUpdates.resolvers.ts
+++ src/messages/roomUpdates/roomUpdates.resolvers.ts
@@ -1,10 +1,30 @@
-import pubsub from "../../../pubsub";
+import { withFilter } from "apollo-server";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
+import pubsub from "../../../pubsub";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+        if (!room) {
+          throw new Error("You shall not see this.");
+        }
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
