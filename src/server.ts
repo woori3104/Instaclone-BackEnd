e09b374d350a5f5f0b82a93531ce3e7e453bf2cc
woori3104 <woori.kim.se@gmail.com>
2021-06-21 00:14:35 +0900

    npm audit fix

--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,36 @@
 require("dotenv").config();
-import * as http from "http";
 import * as express from "express";
-import * as logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import {typeDefs, resolvers} from "./schema";
-import { getUser } from "./users/users.utils";
 import client from "./client";
-
+import { getUser } from "./users/users.utils";
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  context: async (ctx) => {
-    if (ctx.req) {
-      return {
-        loggedInUser: await getUser(ctx.req.headers.token),
-        client,
-      };
-    } else {
-      const {
-        connection: { context },
-      } = ctx;
-      return {
-        loggedInUser: context.loggedInUser,
-        client,
-      };
+  playground:true,
+  introspection:true,
+  context: async({req}) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+      client,
     }
   },
-  subscriptions: {
-    onConnect: async ({ token }: { token: string }) => {
-      if (!token) {
-        throw new Error("You can't listen.");
-      }
-      const loggedInUser = await getUser(token);
-      return {
-        loggedInUser,
-        client,
-      };
-    },
-  },
+    
 });
-
 const PORT = process.env.PORT
 
 const app = express();
-app.use(logger("tiny"));
-apollo.applyMiddleware({ app });
 
-const httpServer = http.createServer(app);
-apollo.installSubscriptionHandlers(httpServer);
-httpServer.listen({ port: PORT }, () => {
-  console.log(`🚀Server is running on http://localhost:${PORT}/graphql  ✅`);
+
+apollo.applyMiddleware({ app });
+app.get("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.use("/static", express.static("uploads"));
+
+app.listen({ port: PORT }, () => {
+  console.log(`🚀Server is running on http://localhost:${PORT}/graphql ✅`);
 });
